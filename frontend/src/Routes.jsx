ce044e6838464b52af945571babfde04c215e863
haziqhalifi <haziq.halifi@gmail.com>
2025-06-15 11:19:08 +0800

    Add admin dashboard and routing for admin sign-in

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -6,6 +6,8 @@ import Dashboard from './pages/Dashboard';
 import ReportDisaster from './pages/ReportDisaster';
 import EmergencySupport from './pages/EmergencySupport';
 import SignIn from './pages/SignIn';
+import AdminSignIn from './pages/AdminSignIn';
+import AdminDashboard from './pages/AdminDashboard';
 import SignUp from './pages/SignUp';
 import Account from './pages/Account';
 import Settings from './pages/Settings';
@@ -20,6 +22,8 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/signin" element={<SignIn />} />
+        <Route path="/admin/signin" element={<AdminSignIn />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/disaster-map" element={<Dashboard />} />
         <Route path="/report-disaster" element={<ReportDisaster />} />
