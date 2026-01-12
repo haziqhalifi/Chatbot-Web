7f237a30c21fb96bf034f9633a612c668473c9eb
haziqhalifi <haziq.halifi@gmail.com>
2026-01-12 22:07:52 +0800

    update notification

--- frontend/src/pages/admin/Notifications.jsx
+++ frontend/src/pages/admin/Notifications.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
-import Header from '../../components/common/Header';
-import AdminNotificationPanel from '../../components/admin/AdminNotificationPanel';
+import NotificationManagement from './Notifications';
 
 const AdminNotifications = () => {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <Header />
-      <div className="container mx-auto px-4 py-8">
-        <AdminNotificationPanel />
-      </div>
-    </div>
-  );
+  return <NotificationManagement />;
 };
 
 export default AdminNotifications;
